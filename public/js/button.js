729522ac18ccf04d8a642d1aa8edcc1a9d11eb83
s-jin721 <lsj9134721@gmail.com>
2024-08-25 16:38:52 +0900

    second record project

--- public/js/button.js
+++ public/js/button.js
@@ -4,7 +4,7 @@ const modes = document.getElementsByClassName("s_mode");
 
 let lastClickedButton = 0;
 let con = document.getElementById("s_Daily");
-buttons[0].style.backgroundColor="#fcb101";
+buttons[0].style.backgroundColor="white";
 modes[1].style.display = "none";
 modes[2].style.display = "none";
 for(i=0; i<buttons.length; i++){
@@ -29,7 +29,7 @@ function handleButtonClick(event) {
         }
         con.style.display = "block";
     }
-    clickedButton.style.backgroundColor = "#fcb101";
+    clickedButton.style.backgroundColor = "white";
     lastClickedButton = clickedButton;
 }
 
@@ -39,5 +39,51 @@ function resetButtonColors() {
         modes[i].style.display = "none";
     }
 }
+
+
+
+
+
+const buttons_3 = document.getElementsByClassName("s_button-3");
+const modes_2 = document.getElementsByClassName("s_mode_2");
+
+let lastClickedButton_2 = 0;
+let con_2 = document.getElementById("s_distance");
+buttons_3[0].style.backgroundColor="#E7E7E7";
+modes_2[1].style.display = "none";
+modes_2[2].style.display = "none";
+for(j=0; j<buttons_3.length; j++){
+    buttons_3[j].onclick = handleButtonClick_2;
+}
+
+function handleButtonClick_2(event) {
+    const clickedButton_2 = event.target;
+    if (lastClickedButton_2 !== clickedButton_2) {
+        resetButtonColors_2();
+        if(event.target.value === '0'){
+            con_2 = document.getElementById("s_distance");
+            console.log("1");
+        }
+        else if(event.target.value === '1'){
+            con_2 = document.getElementById("s_velocity");
+            console.log("2");
+        }
+        else if(event.target.value === '2'){
+            con_2 = document.getElementById("s_etc");
+            console.log("3");
+        }
+        con_2.style.display = "block";
+    }
+    clickedButton_2.style.backgroundColor = "#E7E7E7";
+    lastClickedButton_2 = clickedButton_2;
+}
+
+function resetButtonColors_2() {
+    for (let j = 0; j < buttons_3.length; j++) {
+        buttons_3[j].style.backgroundColor = "";
+        modes_2[j].style.display = "none";
+    }
+}
+
 
 
