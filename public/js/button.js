6ec4a550a660b5a5926cf5b2ab472caab324361a
s-jin721 <lsj9134721@gmail.com>
2024-11-29 21:45:44 +0900

    dddddddddddddd

--- public/js/button.js
+++ public/js/button.js
@@ -39,3 +39,36 @@ function resetButtonColors() {
 
 
 
+const buttons_2 = document.getElementsByClassName("s_button-2");
+const modes_2 = document.getElementsByClassName("s_mode_2");
+
+let lastClickedButton_2 = null;
+let con_2 = document.getElementById("s_mine");
+buttons_2[0].style.backgroundColor = "#A4E3D8";
+modes_2[1].style.display = "none";
+
+for (let j = 0; j < buttons_2.length; j++) {
+    buttons_2[j].onclick = handleButtonClick_2;
+}
+
+function handleButtonClick_2(event_2) {
+    const clickedButton_2 = event_2.target;
+    if (lastClickedButton_2 !== clickedButton_2) {
+        resetButtonColors_2();
+        if (clickedButton_2.getAttribute('value') === '0') {
+            con_2 = document.getElementById("s_mine");
+        } else if (clickedButton_2.getAttribute('value') === '1') {
+            con_2 = document.getElementById("s_new");
+        }
+        con_2.style.display = "block";
+    }
+    clickedButton_2.style.backgroundColor = "#A4E3D8";
+    lastClickedButton_2 = clickedButton_2;
+}
+
+function resetButtonColors_2() {
+    for (let j = 0; j < buttons_2.length; j++) {
+        buttons_2[j].style.backgroundColor = "#698882";
+        modes_2[j].style.display = "none";
+    }
+}
